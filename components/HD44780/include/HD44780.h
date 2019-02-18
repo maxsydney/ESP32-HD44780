c7e7d292b4a9acaabd79464f9045ca5b95afe5b3
Max Sydney <mjs364@uclive.ac.nz>
2019-02-18 21:34:55 +1300

    Changed file structure to a standalone component. Updated readme

--- components/HD44780/include/HD44780.h
+++ components/HD44780/include/HD44780.h
@@ -5,4 +5,4 @@ void LCD_setCursor(uint8_t col, uint8_t row);
 void LCD_home(void);
 void LCD_clearScreen(void);
 void LCD_writeChar(char c);
-void LCD_writeStr(char* str);
+void LCD_writeStr(char* str); 
